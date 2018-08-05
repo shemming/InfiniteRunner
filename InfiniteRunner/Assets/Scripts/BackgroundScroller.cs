ae04ed242cd6087c758aedee76987922e8913a02
Sabrina <shemming444@gmail.com>
2018-08-05 13:47:46 -0400

    Create scrolling background

---
+++ InfiniteRunner/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        Debug.Log( startPosition.ToString() );
+    }
+
+    void Update()
+    {
+        var newPosition = Mathf.Repeat( Time.time * scrollSpeed, tileSizeZ );
+        transform.position = startPosition + Vector3.left * newPosition;
+    }
+}
